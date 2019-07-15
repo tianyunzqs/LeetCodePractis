3a01bd53071f7e409589541d6011d190e0c89fdf
zhuqisi <qszhu@fiberhome.com>
2019-07-15 15:47:05 +0800

    新增递归解法

--- leetcode_21_40/21_mergeTwoLists.py
+++ leetcode_21_40/21_mergeTwoLists.py
@@ -24,15 +24,27 @@ class ListNode:
         self.next = None
 
 
-def buildList(numbers: list) -> ListNode:
-    head = ListNode(numbers[0])
-    current = head
-    for num in numbers[1:]:
-        newNode = ListNode(num)
-        current.next = newNode
-        current = newNode
+def build_linklist(val_list: list) -> ListNode:
+    if not val_list:
+        return None
+
+    head = ListNode(val_list[0])
+    tp = head
+    for val in val_list[1:]:
+        newNode = ListNode(val)
+        tp.next = newNode
+        tp = newNode
 
     return head
+
+
+def print_linklist(head: ListNode):
+    val_list = []
+    while head:
+        val_list.append(str(head.val))
+        head = head.next
+
+    print('->'.join(val_list))
 
 
 def traverse(head: ListNode) -> list:
@@ -54,7 +66,59 @@ def mergeTwoLists(l1: ListNode, l2: ListNode) -> ListNode:
     if not allNumbers:
         return None
 
-    return buildList(allNumbers)
+    return build_linklist(allNumbers)
 
 
+def mergeTwoLists2(l1: ListNode, l2: ListNode) -> ListNode:
+    """
+     递归解法
+     递归的核心方法是将问题规模不断缩小化
+     合并两个长度为n和m的链表，在value(n) < value(m)可以将规模缩减为合并长度为(n-1)和m的链表
+    """
+    if not l1:
+        return l2
+    if not l2:
+        return l1
 
+    if l1.val < l2.val:
+        l1.next = mergeTwoLists2(l1.next, l2)
+        return l1
+    else:
+        l2.next = mergeTwoLists2(l1, l2.next)
+        return l2
+
+
+def mergeTwoLists3(l1: ListNode, l2: ListNode) -> ListNode:
+    """
+     遍历解法(有错误)
+     同时不断遍历两个链表，取出小的追加到新的头节点后，直至两者其中一个为空
+     再将另一者追加的新链表最后
+    """
+    dummy = ListNode(-1)
+    cur = dummy
+    while l1 and l2:
+        if l1.val <= l2.val:
+            cur.next = l1
+            l1 = l1.next
+        else:
+            cur.next = l2
+            l2 = l2.next
+        cur = cur.next
+
+    cur.next = l1 if l1 else l2
+
+    return dummy.next
+
+
+if __name__ == '__main__':
+    a = [1, 2, 4]
+    b = [1, 3, 4]
+    l1 = build_linklist(a)
+    l2 = build_linklist(b)
+    print_linklist(l1)
+    print_linklist(l2)
+
+    new_head2 = mergeTwoLists2(l1, l2)
+    new_head3 = mergeTwoLists3(l1, l2)
+    print_linklist(new_head2)
+    print_linklist(new_head3)
