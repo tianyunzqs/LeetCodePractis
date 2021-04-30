f9abfc737525a4b35dfa85bba0d0bf44af7a0a5f
fhpt <tianyunzqs@sina.com>
2021-04-30 13:50:36 +0800

    第116题：填充每个节点的下一个右侧节点指针

---
+++ tree/116_connect.py
@@ -0,0 +1,317 @@
+# -*- coding: utf-8 -*-
+# @Time        : 2021/4/30 11:18
+# @Author      : tianyunzqs
+# @Description :
+
+"""
+https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node/
+116. 填充每个节点的下一个右侧节点指针
+给定一个 完美二叉树 ，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+初始状态下，所有 next 指针都被设置为 NULL。
+
+进阶：
+你只能使用常量级额外空间。
+使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+
+示例：
+输入：root = [1,2,3,4,5,6,7]
+输出：[1,#,2,3,#,4,5,6,7,#]
+解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。
+序列化的输出按层序遍历排列，同一层节点由 next 指针连接，'#' 标志着每一层的结束。
+
+提示：
+树中节点的数量少于 4096
+-1000 <= node.val <= 1000
+"""
+
+import uuid
+from random import sample
+from typing import List
+from graphviz import Digraph
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+def build_tree(nums, cnt=[0]):
+    """
+    创建二叉树
+    :param nums: 根据前序遍历整理的数组，空节点用None表示，叶子节点的左右节点都是None
+    :param cnt: 计数器
+    :return:
+    """
+    if not nums:
+        return None
+
+    x = nums[cnt[0]]
+    cnt[0] += 1
+    if x is None:
+        bt = None
+    else:
+        bt = TreeNode(x)
+        bt.left = build_tree(nums, cnt)
+        bt.right = build_tree(nums, cnt)
+
+    return bt
+
+
+def build_tree_node(nums, cnt=[0]):
+    """
+    创建二叉树
+    :param nums: 根据前序遍历整理的数组，空节点用None表示，叶子节点的左右节点都是None
+    :param cnt: 计数器
+    :return:
+    """
+    if not nums:
+        return None
+
+    x = nums[cnt[0]]
+    cnt[0] += 1
+    if x is None:
+        bt = None
+    else:
+        bt = Node(x)
+        bt.left = build_tree_node(nums, cnt)
+        bt.right = build_tree_node(nums, cnt)
+
+    return bt
+
+
+def print_tree_preorder(root: TreeNode):
+    """
+    前序遍历
+    :param root: 二叉树的根节点
+    :return:
+    """
+    res = []
+
+    def preoder(root):
+        if root:
+            res.append(root.val)
+            preoder(root.left)
+            preoder(root.right)
+        else:
+            res.append(None)
+
+    preoder(root)
+    return res
+
+
+def print_tree_inorder(root: TreeNode):
+    """
+    中序遍历
+    :param root: 二叉树的根节点
+    :return:
+    """
+    res = []
+
+    def inoder(root):
+        if root:
+            inoder(root.left)
+            res.append(root.val)
+            inoder(root.right)
+        else:
+            res.append(None)
+
+    inoder(root)
+    return res
+
+
+def print_tree_postorder(root: TreeNode):
+    """
+    后序遍历
+    :param root: 二叉树的根节点
+    :return:
+    """
+    res = []
+
+    def postoder(root):
+        if root:
+            postoder(root.left)
+            postoder(root.right)
+            res.append(root.val)
+        else:
+            res.append(None)
+
+    postoder(root)
+    return res
+
+
+def print_tree_levelorder1(root: TreeNode):
+    """
+    层序遍历
+    :param root: 二叉树的根节点
+    :return:
+    """
+    res = []
+    if not root:
+        return []
+    queue = [root]
+    while len(queue) > 0:  # 直到队列中节点全部被取出
+        node = queue.pop(0)   # 先入对列先出（模拟队列）
+        res.append(node.val)
+        # 左右孩子节点都入列队，出队列的时候，按先进先出原则
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+    return res
+
+
+def print_tree_levelorder(root: TreeNode):
+    """
+    层序遍历
+    :param root: 二叉树的根节点
+    :return:
+    """
+    res = []
+    if not root:
+        return []
+    queue = [root]
+    while len(queue) > 0:  # 直到队列中节点全部被取出
+        tmp = []
+        level_size = len(queue)
+        for i in range(level_size):  # 队列中保存的是每一层的节点
+            node = queue.pop(0)   # 先入对列先出（模拟队列）
+            tmp.append(node.val)
+            # 左右孩子节点都入列队，出队列的时候，按先进先出原则
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        res.append(tmp)
+    return res
+
+
+def print_tree_height(root: TreeNode):
+    """
+    二叉树的高度
+    :param root:
+    :return:
+    """
+    if not root:
+        return 0
+    left_height = print_tree_height(root.left)
+    right_height = print_tree_height(root.right)
+    return left_height + 1 if left_height > right_height else right_height + 1
+
+
+# 利用Graphviz实现二叉树的可视化
+def view_tree(root, save_path='Binary_Tree.gv', label=False):
+    dot = Digraph(comment='Binary Tree')
+    # colors for labels of nodes
+    colors = ['skyblue', 'tomato', 'orange', 'purple', 'green', 'yellow', 'pink', 'red']
+
+    # 绘制以某个节点为根节点的二叉树
+    def print_node(node, node_tag):
+        # 节点颜色
+        color = sample(colors, 1)[0]
+        if node.left is not None:
+            left_tag = str(uuid.uuid1())            # 左节点的数据
+            dot.node(left_tag, str(node.left.val), style='filled', color=color)    # 左节点
+            label_string = 'L' if label else ''    # 是否在连接线上写上标签，表明为左子树
+            dot.edge(node_tag, left_tag, label=label_string)   # 左节点与其父节点的连线
+            print_node(node.left, left_tag)
+
+        if node.right is not None:
+            right_tag = str(uuid.uuid1())
+            dot.node(right_tag, str(node.right.val), style='filled', color=color)
+            label_string = 'R' if label else ''  # 是否在连接线上写上标签，表明为右子树
+            dot.edge(node_tag, right_tag, label=label_string)
+            print_node(node.right, right_tag)
+
+    # 如果树非空
+    if root.val is not None:
+        root_tag = str(uuid.uuid1())                # 根节点标签
+        dot.node(root_tag, str(root.val), style='filled', color=sample(colors, 1)[0])     # 创建根节点
+        print_node(root, root_tag)
+
+    dot.render(save_path)
+    # dot.view(save_path)
+
+
+class Solution:
+    def level_order(self, root: TreeNode):
+        res = []
+        queue = [root]
+        while len(queue):
+            queue_size = len(queue)
+            one_level_result = []
+            for i in range(queue_size):
+                node = queue.pop(0)
+                one_level_result.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(one_level_result)
+        return res
+
+    def tree_preorder(self, root: Node):
+        """
+        前序遍历
+        :param root: 二叉树的根节点
+        :return:
+        """
+        res = []
+
+        def preoder(root):
+            if root:
+                res.append((root.val, root.next.val if root.next else None))
+                preoder(root.left)
+                preoder(root.right)
+            else:
+                res.append(None)
+
+        preoder(root)
+        return res
+
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return None
+        queue = [root]
+        while len(queue) > 0:
+            queue_size = len(queue)
+            one_level_result = []
+            for _ in range(queue_size):
+                node = queue.pop(0)
+                one_level_result.append(node)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            for i in range(len(one_level_result) - 1):
+                one_level_result[i].next = one_level_result[i+1]
+            one_level_result[-1].next = None
+        return root
+
+
+if __name__ == '__main__':
+    nums = [1, 2, 4, None, None, 5, None, None, 3, 6, None, None, 7, None, None]
+    # nums = [1, 2, None, None, 3, None, None]
+    # nums = [1, 2, None, None, None]
+    nums = []
+    tree_root = build_tree_node(nums, [0])
+    print(Solution().tree_preorder(tree_root))
+    print(Solution().connect(tree_root))
