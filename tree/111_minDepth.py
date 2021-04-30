1c7b3b57e7d5c2b02e2b74abc161d535dc7b5fc6
fhpt <tianyunzqs@sina.com>
2021-04-30 08:47:08 +0800

    第111题：二叉树的最小深度

---
+++ tree/111_minDepth.py
@@ -0,0 +1,238 @@
+# -*- coding: utf-8 -*-
+# @Time        : 2021/4/29 15:31
+# @Author      : tianyunzqs
+# @Description :
+
+"""
+https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/
+111. 二叉树的最小深度
+给定一个二叉树，找出其最小深度。
+最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+说明：叶子节点是指没有子节点的节点。
+
+示例 1：
+输入：root = [3,9,20,null,null,15,7]
+输出：2
+
+示例 2：
+输入：root = [2,null,3,null,4,null,5,null,6]
+输出：5
+
+提示：
+树中节点数的范围在 [0, 10^5] 内
+-1000 <= Node.val <= 1000
+"""
+
+import sys
+import uuid
+from random import sample
+from typing import List
+from graphviz import Digraph
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+def build_tree(nums, cnt=[0]):
+    """
+    创建二叉树
+    :param nums: 根据前序遍历整理的数组，空节点用None表示，叶子节点的左右节点都是None
+    :param cnt: 计数器
+    :return:
+    """
+    if not nums:
+        return None
+
+    x = nums[cnt[0]]
+    cnt[0] += 1
+    if x is None:
+        bt = None
+    else:
+        bt = TreeNode(x)
+        bt.left = build_tree(nums, cnt)
+        bt.right = build_tree(nums, cnt)
+
+    return bt
+
+
+def print_tree_preorder(root: TreeNode):
+    """
+    前序遍历
+    :param root: 二叉树的根节点
+    :return:
+    """
+    res = []
+
+    def preoder(root):
+        if root:
+            res.append(root.val)
+            preoder(root.left)
+            preoder(root.right)
+        else:
+            res.append(None)
+
+    preoder(root)
+    return res
+
+
+def print_tree_inorder(root: TreeNode):
+    """
+    中序遍历
+    :param root: 二叉树的根节点
+    :return:
+    """
+    res = []
+
+    def inoder(root):
+        if root:
+            inoder(root.left)
+            res.append(root.val)
+            inoder(root.right)
+        else:
+            res.append(None)
+
+    inoder(root)
+    return res
+
+
+def print_tree_postorder(root: TreeNode):
+    """
+    后序遍历
+    :param root: 二叉树的根节点
+    :return:
+    """
+    res = []
+
+    def postoder(root):
+        if root:
+            postoder(root.left)
+            postoder(root.right)
+            res.append(root.val)
+        else:
+            res.append(None)
+
+    postoder(root)
+    return res
+
+
+def print_tree_levelorder1(root: TreeNode):
+    """
+    层序遍历
+    :param root: 二叉树的根节点
+    :return:
+    """
+    res = []
+    if not root:
+        return []
+    queue = [root]
+    while len(queue) > 0:  # 直到队列中节点全部被取出
+        node = queue.pop(0)   # 先入对列先出（模拟队列）
+        res.append(node.val)
+        # 左右孩子节点都入列队，出队列的时候，按先进先出原则
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+    return res
+
+
+def print_tree_levelorder(root: TreeNode):
+    """
+    层序遍历
+    :param root: 二叉树的根节点
+    :return:
+    """
+    res = []
+    if not root:
+        return []
+    queue = [root]
+    while len(queue) > 0:  # 直到队列中节点全部被取出
+        tmp = []
+        level_size = len(queue)
+        for i in range(level_size):  # 队列中保存的是每一层的节点
+            node = queue.pop(0)   # 先入对列先出（模拟队列）
+            tmp.append(node.val)
+            # 左右孩子节点都入列队，出队列的时候，按先进先出原则
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        res.append(tmp)
+    return res
+
+
+def print_tree_height(root: TreeNode):
+    """
+    二叉树的高度
+    :param root:
+    :return:
+    """
+    if not root:
+        return 0
+    left_height = print_tree_height(root.left)
+    right_height = print_tree_height(root.right)
+    return left_height + 1 if left_height > right_height else right_height + 1
+
+
+# 利用Graphviz实现二叉树的可视化
+def view_tree(root, save_path='Binary_Tree.gv', label=False):
+    dot = Digraph(comment='Binary Tree')
+    # colors for labels of nodes
+    colors = ['skyblue', 'tomato', 'orange', 'purple', 'green', 'yellow', 'pink', 'red']
+
+    # 绘制以某个节点为根节点的二叉树
+    def print_node(node, node_tag):
+        # 节点颜色
+        color = sample(colors, 1)[0]
+        if node.left is not None:
+            left_tag = str(uuid.uuid1())            # 左节点的数据
+            dot.node(left_tag, str(node.left.val), style='filled', color=color)    # 左节点
+            label_string = 'L' if label else ''    # 是否在连接线上写上标签，表明为左子树
+            dot.edge(node_tag, left_tag, label=label_string)   # 左节点与其父节点的连线
+            print_node(node.left, left_tag)
+
+        if node.right is not None:
+            right_tag = str(uuid.uuid1())
+            dot.node(right_tag, str(node.right.val), style='filled', color=color)
+            label_string = 'R' if label else ''  # 是否在连接线上写上标签，表明为右子树
+            dot.edge(node_tag, right_tag, label=label_string)
+            print_node(node.right, right_tag)
+
+    # 如果树非空
+    if root.val is not None:
+        root_tag = str(uuid.uuid1())                # 根节点标签
+        dot.node(root_tag, str(root.val), style='filled', color=sample(colors, 1)[0])     # 创建根节点
+        print_node(root, root_tag)
+
+    dot.render(save_path)
+    # dot.view(save_path)
+
+
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        if not root.left and not root.right:
+            return 1
+        min_depth = sys.maxsize
+        # 分别计算左右子树的最小深度
+        if root.left:
+            min_depth = min(self.minDepth(root.left), min_depth)
+        if root.right:
+            min_depth = min(self.minDepth(root.right), min_depth)
+        return min_depth + 1
+
+
+if __name__ == '__main__':
+    nums = [3, 9, None, None, 20, 17, None, None, 7, None, None]
+    nums = [1, 2, 3, 4, None, None, 4, None, None, 3, None, None, 2, None, None]
+    nums = [1, 2, 3, None, 4, None, None, None, 2, 3, None, None, 4, None, None]
+    tree_root = build_tree(nums, [0])
+    print(print_tree_levelorder(tree_root))
+    print(print_tree_height(tree_root))
+    print(Solution().minDepth(tree_root))
